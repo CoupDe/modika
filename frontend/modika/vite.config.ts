6c2810e75bed6f952050d680826e7b38f0994851
Artem Troy <coupdegr@gmail.com>
2023-01-26 16:23:03 +0300

    modika start

---
+++ frontend/modika/vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import envCompatible from "vite-plugin-env-compatible";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  envPrefix: "REACT_APP_",
+
+  plugins: [react()],
+});
